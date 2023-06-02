612b7b04da374cc3c22cbb7579dc37838f6417be
Ivo <ivailo.yordano.ph@gmail.com>
2023-06-02 23:43:00 +0300

    last one done

--- Homework_12/src/Main.java
+++ Homework_12/src/Main.java
@@ -1,4 +1,6 @@
+import java.awt.image.RasterOp;
 import java.util.Arrays;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,14 +12,12 @@ public class Main {
 //        System.out.println(Arrays.toString(resultTask1));
 
 
-
 //        int[][] matrixTask2 = {
 //                {2, 4, 5, 1},
 //                {3, 7, 8, 5},
 //                {9, 5, 0, 4}};
 //        int[] resultTask2 = Task_2(matrixTask2);
 //        System.out.println(Arrays.toString(resultTask2));
-
 
 
 //        int[][] matrix1Task3 = {
@@ -33,8 +33,12 @@ public class Main {
 //        }
 
 
-
-//        Task_4();  // todo
+        String[][] board = {
+                {" _ ", " _ ", " _ "},
+                {" _ ", " _ ", " _ "},
+                {" _ ", " _ ", " _ "}
+        };
+        playGame(board);
     }
 
     public static int[] Task_1(int[] array) {
@@ -126,7 +130,72 @@ public class Main {
 
     }
 
-    public static void Task_4(String[] args) {
+
+    // Task 4 below:
+    public static void printBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isWin(String[][] board) {
+
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0]).equals((board[i][1])) && (board[i][0]).equals((board[i][2])) && !(board[i][0]).equals(" _ ")) {
+                System.out.println("T4");
+                return true;
+            }
+        }
+
+        // check cols
+        for (int i = 0; i < 3; i++) {
+            if ((board[0][i]).equals((board[1][i])) && (board[0][i]).equals((board[2][i])) && !(board[0][i]).equals(" _ ")) {
+                System.out.println("T3"+ i + (board[0][i]) + (board[1][i]) + (board[2][i]));
+                return true;
+            }
+        }
+
+        // check diagonal "\"
+        if ((board[0][0]).equals((board[1][1])) && ((board[0][0]).equals((board[2][2])) && !(board[0][0]).equals(" _ "))) {
+            System.out.println("T1");
+            return true;
+        }
+
+        // check diagonal "/"
+        if ((board[0][2]).equals((board[1][1])) && ((board[0][2]).equals((board[2][0])) && !(board[0][2]).equals(" _ "))) {
+            System.out.println("T2");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isBoardFull(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[1].length; j++) {
+                if ((board[i][j]).equals(" _ ")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPossible(String[][] board, int a, int b) {
+        if (a < 1 || a > 3 || b < 1 || b > 3) {
+            System.out.println("Позицията която избрахте е извън фигурата. Пробвай пак!");
+            return false;
+        } else if (!(board[a - 1][b - 1]).equals(" _ ")) {
+            System.out.println("Позицията вече е заета. Пробвай пак!");
+            return false;
+        }
+        return true;
+    }
+
+    public static void playGame(String[][] board) {
         //Бонус задача - не е задължителна
         //Реализирайте играта “морски шах”:
         //Имаме матрица 3х3.
@@ -138,6 +207,49 @@ public class Main {
         //празните места има символи .
         //Когато играч спечели, в конзолата се изписва: Player X wins! (където Х
         //е 1 или 2)
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("МОРСКИ ШАХ");
+        String symbol;
 
+        while (true) {
+            if (isWin(board) || isBoardFull(board)) {
+                break;
+            }
+            int player = 0;
+            symbol = " o ";
+            int hasChanged = 0;
+            while (true) {
+
+                if (player % 2 == 1) {
+                    symbol = " x ";
+                }
+
+                printBoard(board);
+                System.out.print("Играч " + (player + 1) + " [" + symbol + "]: Въведете две числа - ред и колона, където да поставите символ:");
+
+                String userInput = scanner.nextLine();
+
+                int a = Integer.parseInt(userInput.split(" ")[0]);
+                int b = Integer.parseInt(userInput.split(" ")[1]);
+
+                if (isPossible(board, a, b)) {
+                    board[a - 1][b - 1] = symbol;
+                    if (isWin(board)) {
+                        System.out.println("Player " + (player + 1) + " WON the game!!");
+                        break;
+                    }
+                    player += 1;
+                    hasChanged += 1;
+                }
+
+                if (symbol.equals(" x ") && hasChanged == 2) {
+                    break;
+                }
+                if (isBoardFull(board)) {
+                    System.out.println("Няма свободни позиции. Никой не печели играта.");
+                    break;
+                }
+            }
+        }
+    }
 }
