1f7023b68ffeb0e32f285bbbe0705c3745497b72
Ivo <ivailo.yordano.ph@gmail.com>
2023-06-11 11:24:40 +0300

    date check done

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Scanner;
-import java.util.HashMap; // import the HashMap class
+import java.util.*;
 
 
 //Склад
@@ -18,19 +15,18 @@ import java.util.HashMap; // import the HashMap class
 //мерна единица (килограми, литри)
 //налично количество
 //местоположение (секция/рафт/номер) (номерирайте склада си както прецените, че ще ви е удобно,
-// имайте предвид, че в началото той е празен и различно количество стока е нормално да заема
-// различно по обем място)
+// имайте предвид, че в началото той е празен и различно количество стока е нормално да заема различно по обем място)
 //допустим брой от продукта на рафт
 //коментар (свободен текст)
+//
 //записване на нова доставка
-//ако нов продукт е с различен срок на годност от вече съществуващ едноименен продукт,
-// той да бъде поставен на различно място
-//ако имате достатъчно място, еднакви продукти с един и същи срок на годност да бъдат
-// поставени на едно и също място
-//при извеждане на списъка с налични продукти да се изведе общото количество на едноименните
-// продукти независимо от срока им на годност
+//ако нов продукт е с различен срок на годност от вече съществуващ едноименен продукт, той да бъде поставен на различно място
+//ако имате достатъчно място, еднакви продукти с един и същи срок на годност да бъдат поставени на едно и също място
+
+//при извеждане на списъка с налични продукти да се изведе общото количество на едноименните продукти независимо от срока им на годност
 //справка за наличността в даден период (по дадена начална и крайна дата се извежда списък с
 // всички промени на наличността в дадения период, включително зареждания и извеждания на стоки)
+//
 //Пример:
 //Please choose what to do (1 - List all items; 2 - Add new delivery; 3 - List deliveries for time period)
 //<< 1
@@ -66,17 +62,23 @@ import java.util.HashMap; // import the HashMap class
 //List of product transactions for the period 01.05.2021 - 31.05.2021:
 //Light bulb - LED 75W | Entry date: 05.05.2021 | Unit: Item | Stock delivered: 200
 
+// todo - Expiry date: n/a | Entry date: 05.05.2021 - expirity date can be n/a or null
+// todo - readFromDB , writeToDB
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 //            getData();
-        if (positionChecker("A3 /4 / 10")) {
-            System.out.println("y");
-        } else {
-            System.out.println("n");
-        }
+//        if (positionChecker("A3 /4 / 10")) {
+//            System.out.println("y");
+//        } else {
+//            System.out.println("n");
+//        }
+
+
     }
+
     // todo checker - [][][] or letter[][][]
     // todo String unit --> boolean unit --> килограми, литри
     public static String readString(String question) {
@@ -124,13 +126,112 @@ public class Main {
         */
     }
 
+
+    public static boolean dateValidator(String date) {
+        // String date will be in format "dd.mm.yyyy"
+        // Will return True if date is valid, else False.
+
+        String[] parts = date.split("\\.");
+
+        // Check if the formatting is correct - 2 dots -> xx.yy.zzzz
+        if (parts.length != 3) {
+            return false;
+        }
+
+        // Check if all parts can be converted to integers and all are bigger than 1.
+        // Day, month or year can't be 0.
+        for (int i = 0; i < 3; i++) {
+            try {
+                int num = Integer.parseInt(parts[i]);
+                if (num < 1) {
+                    return false;
+                }
+            } catch (Exception e) {
+                System.out.println(parts[i] + " cannot be converted to integer");
+                return false;
+            }
+        }
+
+        int numDay = Integer.parseInt(parts[0]);
+        int numMonth = Integer.parseInt(parts[1]);
+        int numYear = Integer.parseInt(parts[2]);
+
+        // Month should be 1-12
+        if (numMonth > 12) {
+            return false;
+        }
+
+        // Year should be 4 digits long - ex. 2021
+        if (parts[2].length() != 4) {
+            return false;
+        }
+
+        int daysInCurrentMonth = getDaysInMonth(numYear, numMonth);
+
+        // Check if the month has that date.
+        if (numDay > daysInCurrentMonth) {
+            return false;
+        }
+
+        return true;
+    }
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else return year % 100 != 0;
+    }
+    public static int getDaysInMonth(int year, int month) {
+        // A leap year occurs nearly every 4 years which adds an extra day to February.
+        // February is the only month with 28 days.
+        // There are 30 days in April, June, September and November.
+        // There are 31 days in January, March, May, July, August, October, December.
+
+        int[] months30Days = {4, 6, 9, 11};
+        int[] months31Days = {1, 3, 5, 7, 8, 10, 12};
+
+        if (isCurrentMonthInArray(months30Days, month)) {
+            return 30;
+        }
+
+        if (isCurrentMonthInArray(months31Days, month)) {
+            return 31;
+        }
+
+        // Feb
+        if (isLeapYear(year)) {
+            return 29;
+        } else {
+            return 28;
+        }
+    }
+    public static boolean isCurrentMonthInArray(int[] months, int month) {
+        for (int m : months) {
+            if (m == month) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean unitChecker(String unit) {
+
+    }
+
     //Light bulb - LED 75W | Expiry date: n/a | Entry date: 05.05.2021 |
     // Manufacturer: Philips | Unit: Item | Stock: 104 |
     // Position: A3 / 4 / 10 | Available items at shelf: 500 | Comment:
     public static void getData(String[] questions, String[] keys) {
-        String[] listQuestions = new String[]{"име на продукта", "в 2"};
-        String[] listKeys = new String[]{"name", "expiryDate", "entryDate", "manufacturer",
-                "unit", "stock", "quantity", "position", "itemsPerShelf", "comment"};
+        String[] listQuestions = new String[]{"product name", "expiry date", "entry date", "manufacturer", "unit",
+                "available stock", "comment (optional)"};
+
+        String[] listKeys = new String[]{"name", "expiryDate", "entryDate", "manufacturer", "unit",
+                "stock", "quantity", "position", "itemsPerShelf", "comment"};
+
+        System.out.println("Enter " + question + ":"); // todo
+
         // 1: name
         // 2: expiryDate
         // 3: entryDate
