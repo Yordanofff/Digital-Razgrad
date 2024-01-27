826d7d124f0bcc7074edef92805ffeadf6d0e6df
Yordanoff <ivailo.yordanov.ph@gmail.com>
2024-01-27 14:32:59 +0200

    6 - Mesi - done

--- _Module_3/Homeworks/6/H6/src/main/java/com/example/servingwebcontent/Messi.java
+++ _Module_3/Homeworks/6/H6/src/main/java/com/example/servingwebcontent/Messi.java
@@ -1,8 +1,25 @@
 package com.example.servingwebcontent;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Messi {
-    
+
+    @GetMapping("/mesi")
+    public String mesi(){
+        return "mesi";
+    }
+
+    @GetMapping("/mesi/goals")
+    public String mesiGoals(
+            @RequestParam(
+                    name="name", required = false, defaultValue = "World"
+            ) String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting_file";
+    }
+
 }
