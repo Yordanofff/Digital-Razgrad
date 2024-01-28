7b5b8d6de058d69a63a0f0aff9b535442f4062d6
Yordanoff <ivailo.yordanov.ph@gmail.com>
2024-01-28 17:26:56 +0200

    w1

--- _Module_3/Online_class/w1/serving-web-content/src/main/java/com/example/servingwebcontent/HelloApp.java
+++ _Module_3/Online_class/w1/serving-web-content/src/main/java/com/example/servingwebcontent/HelloApp.java
@@ -1,2 +1,33 @@
-package com.example.servingwebcontent;public class HelloApp {
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloApp {
+    @GetMapping("/")
+        public String yo(
+                @RequestParam(
+                        name="name", required = false, defaultValue = "World"
+                ) String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting_file";
+    }
+
+    @GetMapping("/user")
+    public String user(@RequestParam(name="name") String name, @RequestParam(name="age") int age, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "user";
+    }
+
+    @GetMapping("/addnums")
+    public String user(@RequestParam(name="n") int n, @RequestParam(name="m") int m, Model model) {
+        int result = n + m;
+        model.addAttribute("result", result);
+        return "add";
+    }
+
 }
