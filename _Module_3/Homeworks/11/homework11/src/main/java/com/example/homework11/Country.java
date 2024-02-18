8a9c3eff9cb7b79af4bdcda51c6804c1664338d4
Yordanoff <ivailo.yordanov.ph@gmail.com>
2024-02-18 14:50:34 +0200

    Country - checks done.

--- _Module_3/Homeworks/11/homework11/src/main/java/com/example/homework11/Country.java
+++ _Module_3/Homeworks/11/homework11/src/main/java/com/example/homework11/Country.java
@@ -1,7 +1,10 @@
 package com.example.homework11;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,10 +20,13 @@ public class Country {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Size(min=2, max=20)
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Field cannot be empty")
     private String name;
 
+    @Min(0)
+    @Max(1999)
     @Column(nullable = false)
     private double populationInMillions;
 
